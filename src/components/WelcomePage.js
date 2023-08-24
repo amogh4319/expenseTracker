aa7ba95446dc99ae95a9d78619b89c93d0598721
amogh4319 <amogh4319@gmail.com>
2023-08-24 13:57:39 +0530

    adding logout

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './WelcomePage.css';
-import { Card} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, Card} from 'react-bootstrap';
+import { Link,useNavigate } from 'react-router-dom';
+import AuthContext from '../store/authContext';
 function WelcomePage() {
+  const ctx=useContext(AuthContext);
+  const history=useNavigate();
+  const logoutPage=()=>{
+    ctx.logOut();
+    
+    ctx.token='';
+    
+    history('/');
+  }
   return (
     <div>
         
       <h2><i>Welcome to Expense tracker app</i></h2>
       <Card className='p' style={{backgroundColor:'pink'}}><p><i>your profile is incomplete <Link to='/profile'>Complete now</Link></i></p></Card>
-      
+      <Button variant='danger' onClick={logoutPage}>Log Out</Button>
       <hr/>
       
     </div>
